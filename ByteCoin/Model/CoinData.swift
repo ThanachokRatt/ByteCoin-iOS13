a367d65fb13fa4ff61227c97c2d357c77d489ed4
Thanachok Rattanamora <schweppeweb@gmail.com>
2023-03-06 13:34:07 +0700

    d

--- ByteCoin/Model/CoinData.swift
+++ ByteCoin/Model/CoinData.swift
@@ -1,11 +1,3 @@
-//
-//  CoinData.swift
-//  ByteCoin
-//
-//  Created by Schweppe on 13/2/2566 BE.
-//  Copyright © 2566 BE The App Brewery. All rights reserved.
-//
-
 import Foundation
 struct CoinData : Codable {
     let rate: Double
--- ByteCoin/Model/CoinModel.swift
+++ ByteCoin/Model/CoinModel.swift
@@ -1,11 +1,3 @@
-//
-//  CoinModel.swift
-//  ByteCoin
-//
-//  Created by Schweppe on 13/2/2566 BE.
-//  Copyright © 2566 BE The App Brewery. All rights reserved.
-//
-
 import Foundation
 struct CoinModel {
     let coinRate :Double
